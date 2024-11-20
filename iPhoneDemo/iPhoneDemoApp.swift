40ba18bfe6f98e4268cd5ede598a77f22720ea20
Prakash Bahadur Chand <prakashbahadurchand@gmail.com>
2024-11-20 15:42:42 +0545

    Initial Commit

---
+++ iPhoneDemo/iPhoneDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPhoneDemoApp.swift
+//  iPhoneDemo
+//
+//  Created by Prakash Bahadur Chand on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct iPhoneDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
